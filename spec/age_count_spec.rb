ca254500fd5747b41393d62bd41d0abe8802d1d5
theoretick <me@lucascharles.me>
2013-09-08 12:25:55 -0700

    changed solution wording to age_count in spec

--- spec/age_count_spec.rb
+++ spec/age_count_spec.rb
@@ -1,23 +1,23 @@
-require 'solution'
+require 'age_count'
 
-describe "solution" do
-
-  # these 2 can probably use the same data/output
+describe "age_count" do
 
   describe "initialization" do
-
-    it 'should fail gracefully and do nothing with 0 args' do
-      expect(AgeCount.new).not_to raise_error
-    end
 
     it 'should accept 1 arg' do
       ARGV << 'data/mock_file1.csv'
       expect(AgeCount.new).not_to raise_error
     end
 
-    # it 'should accept STDIN' do
-      # expect().to eq()
-    # end
+    it 'should accept 1 arg from shell' do
+      expect(`./age_count.rb data/tiny_mock_file0.csv`).to eq(
+        "68,1\n62,1\n30,1\n55,1\n21,1\n")
+    end
+
+    it 'should accept STDIN' do
+      expect(`cat data/tiny_mock_file0.csv | ./age_count.rb`).to eq(
+        "68,1\n62,1\n30,1\n55,1\n21,1\n")
+    end
 
     it 'should accept 2 args' do
       ARGV << 'data/mock_file1.csv'
