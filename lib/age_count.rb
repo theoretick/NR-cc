88660cdc11921ac1087e29f86362c99c5d261300
theoretick <me@lucascharles.me>
2013-09-08 19:22:54 -0700

    removed extra comments

--- lib/age_count.rb
+++ lib/age_count.rb
@@ -15,34 +15,31 @@ class AgeCount
   def initialize; end
 
   def run
-    @age_table = Hash.new{0}
+    @age_table = Hash.new{ 0 }
     sum_ages
-    aggregate_ages
+    display_ages
   end
 
-  # builds @age_table for each line of ARGV
-  # - @age_table format: {..., age => age_count, ...}
-  # returns 0
+  # builds @age_table by parsing each line of ARGV
+  # - @age_table format: { age => age_count, ... }
   def sum_ages
     ARGF.each do |in_line|
       sum_age(in_line)
     end
   end
 
-  # parses in_line to increment user_age by 1
-  # returns 0.
+  # parses in_line to increment +1 on user_age entry
   def sum_age(in_line)
-    user_id, raw_user_age = in_line.split(',')
-    # quick-strip of trailing '\n' w/ #to_i
-    user_age = raw_user_age.to_i #quick_strip trailing '\n'
+    user_id, raw_age = in_line.split(',')
+    user_age         = raw_age.to_i #quick_strip trailing '\n'
+
     @age_table[user_age] += 1
   end
 
   # outputs ages with their aggregated counts
   # - aggregates ages as array of 2-item tuples: [age, age_count]
   # returns array of tuples
-  def aggregate_ages
-    # p *@age_table
+  def display_ages
     @age_table.each_pair do |age, age_count|
       puts "%i,%i" % [age, age_count]
     end
